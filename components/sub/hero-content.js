f8662248e5f71cb603e0c5b519e3db6023dcd5dc
Maqsood Ahmed <maqsoodnasirani421@gmail.com>
2025-05-11 15:04:33 +0500

    made it responsive for small devices

--- components/sub/hero-content.js
+++ components/sub/hero-content.js
@@ -30,7 +30,7 @@ export const HeroContent = () => {
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-3xl sm:text-4xl text-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-6 text-3xl sm:text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
             Providing{" "}
