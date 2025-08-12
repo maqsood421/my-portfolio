84754eb5ad746f00c5a1357f505543b916bf06e3
Maqsood Ahmed <maqsoodnasirani421@gmail.com>
2025-08-12 23:09:34 +0500

    removed video from Hero Section

--- components/Hero.js
+++ components/Hero.js
@@ -3,15 +3,6 @@ import { HeroContent } from "./sub/hero-content";
 const Hero = () => {
   return (
     <div id="#about" className="relative flex flex-col h-full w-full">
-      <video
-        autoPlay
-        muted
-        loop
-        className="rotate-180 absolute top-[-340px] left-0 w-full h-full object-cover -z-20"
-      >
-        <source src="videos/blackhole.webm" type="video/webm" />
-      </video>
-
       <HeroContent />
     </div>
   );
